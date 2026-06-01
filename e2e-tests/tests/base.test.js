6d662f0b113d39ad351e64b5bbd702fabd81633c
Thiago Tranquilim <trtranquilim@gmail.com>
2026-06-01 15:31:22 -0300

    feat: adiciona Time 12 IDPJ

--- e2e-tests/tests/base.test.js
+++ e2e-tests/tests/base.test.js
@@ -12,6 +12,7 @@ const runPiscinaTests = require("./Time_10_piscina/piscina-all-screens.test.js")
 const runFinanceTests = require("./financecar/financecar-all-screens.test.js");
 const runMkpTests = require("./mkp/mkp-all-screens.test.js");
 const runSusTests = require("./sus/sus-all-screens.test.js");
+const runIdpjTests = require("./Time_12(IDPJ)/idpj-all-screens.test.js");
 
 const BASE_URL = process.env.APP_URL || "http://localhost:3000";
 const SCREENSHOTS_DIR = path.join(__dirname, "..", "screenshots");
@@ -82,6 +83,8 @@ async function main() {
     await runPiscinaTests();
     console.log("\n--- Iniciando testes do SUS ---");
     await runSusTests(driver);
+    console.log("\n--- Iniciando testes do IDPJ ---");
+    await runIdpjTests();
   } finally {
     if (driver) await driver.quit();
   }
